f3a72069eacf24fc79bb28c176788ac3198a4517
JCessss <56443210+JCessss@users.noreply.github.com>
2019-11-21 17:36:43 -0500

    code to connect to database
    still figuring out how to upload the result of a user into the database rather than manually putting in the information

--- SurveyService/src/main/java/survey/Survey.java
+++ SurveyService/src/main/java/survey/Survey.java
@@ -1,7 +1,58 @@
 package survey;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Survey {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    
+private final String USERNAME = "root";
+private final String PASSWORD = "root";
+private final String CONN = "jdbc:mysql://localhost/surveyme";    
+        
+    
+public static void getConnection() throws SQLException{
+    
+    
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(CONN,USERNAME,PASSWORD);
     }
+        catch (ClassNotFoundException|SQLException ex){
+            ex.printStackTrace();
+       
+
+}
+    return null;
+
+}
+
+    
+    public static void main(String[] args){
+
+        
+String username = "";
+String question = "";
+String[] options = {};
+String[][] answers = {{}};          
+        
+        
+        
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("username?");
+        username = input.nextLine();
+        
+        
+        Connection con = this.getConnection();
+        Statement statement = con.createStatement();
+        statement.executeUpdate("INSERT INTO surveys " + "VALUES ('' , '' , '' , '')");
+        System.out.println("connected");
+    }
+    
+    
 }
